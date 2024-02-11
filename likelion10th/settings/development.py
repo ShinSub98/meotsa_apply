9075534750bcdc8c120c8f08b6beb9b0679b0a2f
ShinSub98 <wjdtjq26@naver.com>
2024-02-11 22:19:14 +0900

    docker 재설정

--- likelion10th/settings/development.py
+++ likelion10th/settings/development.py
@@ -4,22 +4,22 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-LOGGING = {
-    'version' : 1,
-    'disable_existing_loggers': False,
-    'handlers' : {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        }
-    },
-    'loggers': {
-        'django.db.backends': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-    }
-}
+# LOGGING = {
+#     'version' : 1,
+#     'disable_existing_loggers': False,
+#     'handlers' : {
+#         'console': {
+#             'level': 'DEBUG',
+#             'class': 'logging.StreamHandler',
+#         }
+#     },
+#     'loggers': {
+#         'django.db.backends': {
+#             'handlers': ['console'],
+#             'level': 'DEBUG',
+#         },
+#     }
+# }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
