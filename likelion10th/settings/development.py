db36f349c62f64a1b6cce912fe4d9f3e1785ee25
ShinSub98 <wjdtjq26@naver.com>
2024-02-11 21:57:44 +0900

    docker 재설정

--- likelion10th/settings/development.py
+++ likelion10th/settings/development.py
@@ -20,12 +20,23 @@ LOGGING = {
         },
     }
 }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'meotsa_apply',
+        'USER': 'root',
+        'PASSWORD': config('MYSQL_ROOT_PASSWORD'),
+        'HOST': 'mysql_db',
+        'PORT': '3306'
+    }
+}
