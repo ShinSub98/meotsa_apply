5d2f03c72569520b64e16481ced4ed60be932b1d
ShinSub98 <wjdtjq26@naver.com>
2024-02-11 20:25:14 +0900

    fix: server_name 수정

--- likelion10th/settings/production.py
+++ likelion10th/settings/production.py
@@ -3,7 +3,7 @@ from decouple import config
 
 DEBUG = False
 # 추후 ip 추가
-ALLOWED_HOSTS = [ config("ALLOWED_HOST"), "www.likelion-hufs-seoul.com", "likelion-hufs-seoul.com" ]
+ALLOWED_HOSTS = [config("ALLOWED_HOST"), "www.likelion-hufs-seoul.com", "likelion-hufs-seoul.com" ]
 
 
 import pymysql
