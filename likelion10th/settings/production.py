1969120b546ce1fd94adc0b5fffa54e20f84d430
ShinSub98 <wjdtjq26@naver.com>
2024-02-12 18:55:36 +0900

    도메인 추가

--- likelion10th/settings/production.py
+++ likelion10th/settings/production.py
@@ -3,7 +3,7 @@ from decouple import config
 
 DEBUG = True
 # 추후 ip 추가
-ALLOWED_HOSTS = [config("ALLOWED_HOST"), "www.likelion-hufs-seoul.com", "likelion-hufs-seoul.com", "13.124.241.62"]
+ALLOWED_HOSTS = [config("ALLOWED_HOST"), "http://hufs-likelion12.site", "hufs-likelion12.site" "13.124.241.62", "43.203.57.7"]
 
 
 import pymysql
