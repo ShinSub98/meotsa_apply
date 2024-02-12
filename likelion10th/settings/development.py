896f7572f082ed39603e921a2240a20af3553b76
ShinSub98 <wjdtjq26@naver.com>
2024-02-12 17:16:17 +0900

    docker

--- likelion10th/settings/development.py
+++ likelion10th/settings/development.py
@@ -20,12 +20,12 @@ ALLOWED_HOSTS = ["*"]
 #         },
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
@@ -33,13 +33,13 @@ CORS_ALLOW_CREDENTIALS = True
 import pymysql
 pymysql.install_as_MySQLdb()
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql', 
-#         'NAME': 'meotsa_apply',
-#         'USER': 'root',
-#         'PASSWORD': config('MYSQL_ROOT_PASSWORD'),
-#         'HOST': 'mysql_db',
-#         'PORT': '3306'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'meotsa',
+        'USER': 'root',
+        'PASSWORD': config('MYSQL_ROOT_PASSWORD'),
+        'HOST': 'mysql_db',
+        'PORT': '3306'
+    }
+}
